ebb72b6cd25abb248f4b7552aa9b62cff9cf942b
adhamessammetwally@gmail.com <adhamessammetwally@gmail.com>
2023-05-17 12:37:48 +0300

    attempt to fix #3 & #10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,8 +2,7 @@
 
 def best_score(a_dictionary):
     if a_dictionary is None or a_dictionary == {}:
-        return None
-    
+        return None    
     biggest = max(a_dictionary.values())
     for key, value in a_dictionary.items():
         if value is biggest:
