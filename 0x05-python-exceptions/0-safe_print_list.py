05613c7d759145277354c9966602e789641b2305
adhamessammetwally@gmail.com <adhamessammetwally@gmail.com>
2023-05-30 01:36:34 +0300

    Task #0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,8 +5,9 @@ def safe_print_list(my_list=[], x=0):
     words = 0;
     for i in range(0, x):
         try:
-            print("{}".format(my_list[i], end=""))
+            print("{}".format(my_list[i]), end="")
             words += 1
         except:
             continue
+    print()
     return words
