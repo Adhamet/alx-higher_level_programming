45423552d3fee569bd58e408098f9572c757806d
adhamessammetwally@gmail.com <adhamessammetwally@gmail.com>
2023-06-15 16:08:14 +0300

    Task #10 & #11

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -15,3 +15,32 @@ class Square(Rectangle):
         """Returns string info about this square."""
         return '[{}] ({}) {}/{} - {}'.\
             format(type(self).__name__, self.id, self.x, self.y, self.width)
+
+    @property
+    def size(self):
+        """Size of square"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Set size of square"""
+        self.width = value
+        self.height = value
+
+    def __update(self, id=None, size=None, x=None, y=None):
+        """Internal method that updates instance attributes via arguments"""
+        if id is not None:
+            self.id = id
+        if size is not None:
+            self.size = size
+        if x is not None:
+            self.x = x
+        if y is not None:
+            self.y = y
+
+    def update(self, *args, **kwargs):
+        """Updates instance attributes via no-keyword & keyword args"""
+        if args:
+            self.__update(*args)
+        elif kwargs:
+            self.__update(**kwargs)
