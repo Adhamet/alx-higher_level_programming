5509cc4489577648413ba0e787804207b8716017
adhamessammetwally@gmail.com <adhamessammetwally@gmail.com>
2023-06-15 14:09:13 +0300

    Task #1

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This module contains Base class
+"""
+
+
+class Base:
+    """
+    A basic class for up-coming classes
+
+    Attributes: __nb_objects, the current number of objects
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id = None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects = Base.__nb_objects + 1
+            self.id = Base.__nb_objects
