44aa0f4a41544cc9bdfa6380e5b227832b33244c
adhamessammetwally@gmail.com <adhamessammetwally@gmail.com>
2023-06-15 15:42:54 +0300

    Task 8 & 9

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -13,7 +13,7 @@ class Base:
 
     __nb_objects = 0
 
-    def __init__(self, id = None):
+    def __init__(self, id=None):
         if id is not None:
             self.id = id
         else:
--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -90,3 +90,23 @@ class Rectangle(Base):
         return '[{}] ({}) {}/{} - {}/{}'.\
             format(type(self).__name__, self.id, self.x, self.y, self.width,
                    self.height)
+
+    def __update(self, id=None, width=None, height=None, x=None, y=None):
+        """Internal method that updates instances via arguments"""
+        if id is not None:
+            self.id = id
+        if width is not None:
+            self.width = width
+        if height is not None:
+            self.height = height
+        if x is not None:
+            self.x = x
+        if y is not None:
+            self.y = y
+
+    def update(self, *args, **kwargs):
+        """Updates instances attributes via no-keyword or keyword args"""
+        if args:
+            self.__update(*args)
+        elif kwargs:
+            self.__update(**kwargs)
