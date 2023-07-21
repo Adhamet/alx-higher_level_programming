8c00119fcf33bf8a4520e9196ebd0233497fd719
adhamessammetwally@gmail.com <adhamessammetwally@gmail.com>
2023-07-21 13:58:50 +0300

    Task #7

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,9 @@
+-- Creates database hbtn_0d_usa & table cities.
+CREATE database if NOT EXISTS `hbtn_0d_usa`;
+CREATE TABLE if NOT EXISTS `hbtn_0d_usa`.`cities` ( 
+    PRIMARY KEY(`id`),
+    `id`       INT          unique    auto_generated,
+    `state_id` INT          not NULL,
+    `name`     VARCHAR(256) not NULL,
+    FOREIGN KEY(`state_id`) REFERENCES `hbtn_0d_usa`.`states`(`id`)
+);
