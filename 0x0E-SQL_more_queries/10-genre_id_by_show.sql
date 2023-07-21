fb6c3e5756aa47259e44cf84dd78f072767f44cf
adhamessammetwally@gmail.com <adhamessammetwally@gmail.com>
2023-07-21 14:19:48 +0300

    Task #10

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,7 @@
+-- Lists all shows contained in hbtn_0d_tvshows
+-- that have at least one genre linked.
+SELECT show.`title`, genre.`genre_id`
+    FROM `tv_shows` AS show
+        INNER JOIN `tv_shows_genre` AS genre
+        ON show.`id` = genre.`id`
+ORDER BY show.`title` AND genre.`genre_id`;
