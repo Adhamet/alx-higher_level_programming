cfd358a8b750b18cb8a28cebf2eee75ff7596844
adhamessammetwally@gmail.com <adhamessammetwally@gmail.com>
2023-08-25 14:21:24 +0300

    Task #2

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3.10.5
+
+"""
+    A script lists that lists all states of
+    database htbn_0e_0_usa starting with letter N
+"""
+
+import sys, MySQLdb
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[2],
+                         host='localhost',
+                         port=3306)
+cursor = db.cursor()
+cursor.execute("SELECT * FROM states\
+               WHERE name LIKE BINARY 'N%'\
+               ORDER BY id ASC")
+data = cursor.fetchall()
+for row in data:
+    print(row)
+cursor.close()
+db.close()
