2348b7c98d93823fb9858d3cb20b6d9f9d076528
adhamessammetwally@gmail.com <adhamessammetwally@gmail.com>
2023-08-25 15:22:39 +0300

    Final attempt

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,7 +10,7 @@ import sys, MySQLdb
 if __name__ == '__main__':
     db = MySQLdb.connect(user=sys.argv[1],
                          passwd=sys.argv[2],
-                         db=sys.argv[2],
+                         db=sys.argv[3],
                          host='localhost',
                          port=3306)
 cursor = db.cursor()
