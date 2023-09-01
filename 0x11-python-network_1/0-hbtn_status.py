acdae53e4e9dbdc5b8dc2c0769f2fc44c1bd4470
adhamessammetwally@gmail.com <adhamessammetwally@gmail.com>
2023-09-01 07:59:23 +0300

    Slight fixing in fetching url

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     req = Request("https://alx-intranet.hbtn.io/status")
     with urlopen(req) as response:
         body = response.read()
-        print("body response:")
+        print("Body response:")
         print("\t- type: {}".format(type(body)))
         print("\t- content: {}".format(body))
         print("\t- utf8 content: {}".format(body.decode("utf-8")))
