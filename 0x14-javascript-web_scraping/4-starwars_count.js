2e965c33110da77acff48f12e008f123cb5f8a2a
adhamessammetwally@gmail.com <adhamessammetwally@gmail.com>
2023-09-26 20:17:05 +0300

    logic fix added condition

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -11,7 +11,9 @@ request(url, function (err, data, body) {
     for (let result = 0; result < films.length; result++) {
       const characters = films[result].characters;
       for (let j = 0; j < characters.length; j++) {
-        if (characters[j] === 'https://swapi-api.hbtn.io/api/people/18/') { counter += 1; }
+        if (characters[j] === 'https://swapi-api.hbtn.io/api/people/18/' || characters[j] === 'https://swapi-api.hbtn.io/api/people/18/') {
+          counter += 1;
+        }
       }
     }
     console.log(counter);
