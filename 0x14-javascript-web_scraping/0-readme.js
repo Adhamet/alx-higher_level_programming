17574281376af08ffe983f35313c171b486cdf9b
adhamessammetwally@gmail.com <adhamessammetwally@gmail.com>
2023-09-26 19:49:51 +0300

    semistandard fix

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,9 +3,9 @@
 const filePath = process.argv[2];
 const fs = require('fs');
 fs.readFile(filePath, 'utf8', function (err, data) {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log(data);
-    }
-})
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
